74891734e1ff42ebfeb1e34971b0d63abc253aac
Kenzo Rwema <kenzorwema@gmail.com>
2025-02-02 23:05:24 +0200

    Worked on the home page, looks better but still needs a time input and a sign in widget.

--- BusApp/obj/Debug/net8.0/BusApp.AssemblyInfo.cs
+++ BusApp/obj/Debug/net8.0/BusApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b985020244489e1c5937d28edf435fdd7b94bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
